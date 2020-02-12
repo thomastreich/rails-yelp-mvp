a9438465e54860b15a1a27db6ccbed62aa975003
Thomas Streich <tstreich@hotmail.fr>
2020-02-12 14:02:27 +0100

    yelp with a little bit of front

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,13 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+restaurants = Restaurant.create([
+  { name: 'Buffalo Grill', address: '22 chemin des cerises', phone_number: '0622547890', category: 'french' },
+  { name: 'chez Papa', address: '22 chemin des chiens', phone_number: '06228909890', category: 'belgian' },
+  { name: 'Chez ton meilleur pote', address: '22 chemin des rats', phone_number: '06225653890', category: 'chinese'},
+  { name: 'Chez moi', address: '22 chemin des mois', phone_number: '06135653890', category: 'italian'},
+  { name: 'Aux petits', address: '22 chemin des olivzer', phone_number: '06105653890', category: 'japanese'}
+])
