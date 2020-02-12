83e511482169769979217e7b5c062fd8b74c320c
Thomas Streich <tstreich@hotmail.fr>
2020-02-12 12:16:26 +0100

    Yelp without view

---
+++ db/migrate/20200212110435_add_column_to_restaurant.rb
@@ -0,0 +1,5 @@
+class AddColumnToRestaurant < ActiveRecord::Migration[6.0]
+  def change
+    add_column :restaurants, :restaurant_id, :integer
+  end
+end
---
+++ db/migrate/20200212110642_remove_column_to_restaurant.rb
@@ -0,0 +1,5 @@
+class RemoveColumnToRestaurant < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :restaurants, :restaurant_id, :integer
+  end
+end
